b0383c1c6e116ef9248d500ccc430f4992183f54
SMH642800 <SMH642800@gmail.com>
2023-10-02 22:04:16 +0800

    create an ScreenCaptureAPP

--- hao_project_test/google_vision_and_trans_test.py
+++ hao_project_test/google_vision_and_trans_test.py
@@ -1,16 +1,20 @@
+import time
 import os
 from google.cloud import vision_v1
 from google.cloud import translate_v2 as translate
 
 # 設定Google Cloud金鑰環境變數，請將YOUR_GOOGLE_CLOUD_KEY替換成你的實際金鑰
-# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/menghao/Downloads/大學專題資料/googleAPI/manifest-surfer-400014-6ed9f85a5367.json'
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "YOUR_GOOGLE_CLOUD_KEY.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/menghao/Downloads/大學專題資料/googleAPI/manifest-surfer-400014-6ed9f85a5367.json'
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "YOUR_GOOGLE_CLOUD_KEY.json"
 
 # 初始化Google Cloud Vision API客戶端
 client_vision = vision_v1.ImageAnnotatorClient()
 
 # 初始化Google Cloud Translation API客戶端
 client_translate = translate.Client()
+
+# 開始測量時間
+detected_start = time.time()
 
 # 載入圖片
 image_path = "img001.png"  # 將此替換為你的圖片路徑
@@ -25,9 +29,16 @@ texts = response.text_annotations
 # 提取辨識到的文字
 if texts:
     detected_text = texts[0].description
+
+    # detect text (OCR)
+    detected_end = time.time()
+
     print("Detected Text: \n")
     print(detected_text)
     print("----------------------------------------")
+
+    # translation 測量開始
+    trans_start = time.time()
 
     # 將辨識的文字按行分割
     lines = detected_text.split("\n")
@@ -46,7 +57,14 @@ if texts:
     # 將翻譯後的行重新組合成一個帶有換行的字符串
     translated_text_with_newlines = "\n".join(translated_lines)
 
+    # translation 測量結束
+    trans_end = time.time()
+
     print(f"Translated Text ({target_language}): \n{translated_text_with_newlines}")
+
+    # 輸出測量結果
+    print("OCR辨識時間： %f 秒" % (detected_end - detected_start))
+    print("translation時間： %f 秒" % (trans_end - trans_start))
 
 else:
     print("No text detected in the image.")
